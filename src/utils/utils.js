d9a17abad83eaa4ae7d1d9e04415dde7bb1bea31
goldenstateofmind <stateofgoldenmind@gmail.com>
2022-07-27 22:26:44 -0700

    Remove voice speed adjustment

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -32,7 +32,7 @@ export const textToSpeech = ({e, text, language}) => {
   // msg.voice = voices[10] // Note: some voices don't support altering params
   // msg.voiceURI = 'native'
   // msg.volume = 1 // 0 to 1
-  msg.rate = 0.41 // 0.1 to 10
+  // msg.rate = 0.71 // 0.1 to 10
   // msg.pitch = 2 //0 to 2
   msg.text = text
   msg.lang = 'es-MX'
